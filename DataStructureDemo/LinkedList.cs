66f24601bb654fb619f440d9852889ba49a7439b
CMD <gautamkailashsingh@gmail.com>
2020-10-21 23:12:42 +0530

    [Gautam] Added a function to add a value at a particular position.

--- DataStructureDemo/LinkedList.cs
+++ DataStructureDemo/LinkedList.cs
@@ -54,7 +54,34 @@ namespace DataStructureDemo
             Console.WriteLine(temp.data);
         }
 
-        
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
 
     }
 }
--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -9,8 +9,8 @@ namespace DataStructureDemo
             Console.WriteLine("Welcome to linked list program");
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
             list.Display();
         }
     }
